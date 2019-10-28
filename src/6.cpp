e81d638e8b3b0ee7ca221eec0244c5b00d131551
sayhi <1045541554@qq.com>
2019-10-28 12:03:42 +0800

    .

--- src/6.cpp
+++ src/6.cpp
@@ -5,10 +5,27 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     string convert(string s, int numRows) {
-        
+        int length = s.size();
+        int rows = numRows, cols = 0;
+        int gap_cols = numRows - 2;
+
+        vector<vector<int>> str_matrix;
+        vector<int> temp;
+        for(int i = 0; i < s.size(); ++i){
+            if(temp.size() < rows){
+                temp.push_back(s[i]);
+                
+            }
+        }
+
     }
 };
 // @lc code=end
