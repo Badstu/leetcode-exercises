7c42073a7b47a62195f425bc63585432840e10ce
sayhi <1045541554@qq.com>
2020-01-10 21:05:31 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include "src/15.cpp"
+#include "src/16.最接近的三数之和.cpp"
 
 int main(){
     Solution su = Solution();
-    vector<int> nums = {-1,0,1,2,-1,-4};
-    vector<vector<int>> result = su.threeSum(nums);
-    
+    vector<int> nums = {-1, 2, 1, -4};
+    int target = 1;
+    int res = su.threeSumClosest(nums, target);
+
+
     // cout << (3 < 4-1);
 }
---
+++ src/16.最接近的三数之和.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=cpp
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int result = 0;
+
+        return result;
+    }
+};
+// @lc code=end
+
