bd28a81a96f358dfd8c2ca50a35e4b1d7f6269c8
sayhi <1045541554@qq.com>
2020-01-13 17:03:52 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "src/16.最接近的三数之和.cpp"
+#include "src/16.cpp"
 
 int main(){
     Solution su = Solution();
     vector<int> nums = {-1, 2, 1, -4};
     int target = 1;
     int res = su.threeSumClosest(nums, target);
-
+    cout << res;
 
     // cout << (3 < 4-1);
 }
