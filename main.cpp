a71dfe4795645d372c63a28428bca5e128e6e3c7
sayhi <1045541554@qq.com>
2020-03-07 18:12:47 +0800

    .

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
+
+    int a = 10;
+    int b = 111;
+
+    a = 1000;
+    b = 1111;
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++)
             cout << "* ";
