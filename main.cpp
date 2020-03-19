9277dbc029ec92a23b7a58714b6ab5017574f33d
sayhi <1045541554@qq.com>
2020-03-19 19:14:01 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+typedef long long LL;
 
 const int N = 20010;
 int a[N], b[N];
@@ -15,7 +17,24 @@ int main(){
         cin >> b[i];
     }
 
-    
+    sort(a, a + n);
+    sort(b, b + n);
+
+    LL sum_a = 0, sum_b = 0;
+    for(int i = n - 1; i >= n - 3; i--){
+        sum_a += a[i];
+        sum_b += b[i];
+    }
+
+    if(sum_a >= sum_b) cout << sum_a;
+    else cout << sum_b;
 
     return 0;
 }
+
+
+/*
+5
+1 2 3 4 5
+1 2 3 4 6
+*/
