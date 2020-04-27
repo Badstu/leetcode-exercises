346a390c2d2e6a05a07b0d491adb59361cb19bfa
sayhi <1045541554@qq.com>
2020-04-27 12:42:12 +0800

    .

--- src/23.cpp
+++ src/23.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,38 +6,40 @@ using namespace std;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) return NULL;
+
+    ListNode* mergeTwoLists(ListNode* ha, ListNode* hb){
         ListNode* dummy = new ListNode(-1);
         auto head = dummy;
-
-        auto cur = lists[0];
-        int n = lists.size();
-
-        for(int i = 1; i < n; i++){
-            auto p = lists[i];
-            
-            while(cur && p){
-                if(cur -> val < p -> val){
-                    head -> next = cur;
-                    cur = cur -> next;
-                } else{
-                    head -> next = p;
-                    p = p -> next;
-                }
+        while(ha && hb){
+            if(ha -> val <= hb -> val){
+                head -> next = ha;
+                ha = ha -> next;
+                head = head -> next;
+            } else{
+                head -> next = hb;
+                hb = hb -> next;
                 head = head -> next;
             }
-
-            if(cur) head -> next = cur;
-            if(p) head -> next = p;
-
-            cur = dummy -> next;
-            head = dummy;
         }
-
+        if(ha) head -> next = ha;
+        if(hb) head -> next = hb;
         return dummy -> next;
+    }
+
+    ListNode* merge(vector<ListNode*>& lists, int l, int r){
+        if(l > r) return nullptr;
+        if(l == r) return lists[l];
+        int mid = l + r >> 1;
+        auto p = mergeTwoLists(merge(lists, l, mid), merge(lists, mid+1, r));
+        return p;
+    }
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        auto p = merge(lists, 0, n - 1);
+        return p;
     }
 };
