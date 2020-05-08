1de1c13a5065e04baac05d7f343500ed0f8c328e
sayhi <1045541554@qq.com>
2020-05-08 14:54:29 +0800

    .

--- main.cpp
+++ main.cpp
@@ -3,21 +3,53 @@
 using namespace std;
 
 
-int main(){
-    int n;
-    cin >> n;
-
+int getMinimumUniqueSum(vector<int> arr) {
+    if(arr.empty()) return 0;
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    // int i = 0, j = 0;
     int res = 0;
-    while(n >= 10){
-        int t = 1;
-        while(n != 0){
-            t *= n % 10;
-            n /= 10;
+    int tmp = 1;
+    for(int i = 0, j = 1; i < n; i++, j++){
+        while(arr[i] == arr[j]){
+            arr[j] += tmp;
+            tmp++;
+            j++;
         }
-        n = t;
-        res++;
+        res += arr[i];
     }
-    cout << res << endl;
+
+    for(int x: arr){
+        cout << x << " ";
+        res += x;
+    }
+    return res;
+
+}
+
+int main(){
+    // vector<int> arr = {1659, 710, 710, 1730, 1808, 2043, 1613, 1841, 1328, 504, 1730, 2545, 493, 879, 1441, 2043, 1613, 710, 1613, 1250};
+
+    1, 2, 2, 2, 3, 4
+    2, 2, 2, 2, 2, 2
+    o(n^2)
+
+    1, 2, 2, 2, 2, 10, 11
+    2: 4
+    res += (3)!
+    10
+    o(n)
+    4 < 10 - 2
+    {
+        1: 1
+        2: 1
+        3: 1
+        4: 3
+    }
+    sort()
+    int res = getMinimumUniqueSum(arr);
+    cout << res;
+    return 0;
 }
 /*
 1999999999
--- src/1095.cpp
+++ src/1095.cpp
@@ -11,6 +11,32 @@
 class Solution {
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        
+        int n = mountainArr.length();
+        int res = 10000;
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(mountainArr.get(mid) < mountainArr.get(mid+1)){
+                l = mid + 1;
+            } else r = mid;
+        }
+
+        int peak = l;
+        l = 0, r = peak;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(mountainArr.get(mid) >= target) r = mid;
+            else l = mid + 1;
+        }
+        if(mountainArr.get(l) != target){
+            l = peak + 1, r = n - 1;
+            while(l < r){
+                int mid = l + r >> 1;
+                if(mountainArr.get(mid) <= target) r = mid;
+                else l = mid + 1;
+            }
+        }
+
+        return (mountainArr.get(l) == target)? l: -1;
     }
 };
