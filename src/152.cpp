86eb667240ffc56d83ce9a3e062877fb7e2ba244
sayhi <1045541554@qq.com>
2020-05-18 22:32:35 +0800

    .

---
+++ src/152.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+		if(nums.empty()) return 0;
+		int n = nums.size();
+		int max_f = 1, min_f = 1;
+		int ans = INT_MIN;
+		for(int j = 0; j < n; j++){
+			int t_max = max_f, t_min = min_f;
+			max_f = max({t_max * nums[j], t_min * nums[j], nums[j]}); 
+			//一定要选这个数，可以不选前面的数，就从这个数开始，用ans记录下整个过程的最大值
+			min_f = min({t_max * nums[j], t_min * nums[j], nums[j]});
+			ans = max(ans, max_f);
+		}
+		return ans;
+    }
+};
