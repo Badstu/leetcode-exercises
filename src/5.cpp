605f5704217ba999d7487868caf405050be7cf53
sayhi <1045541554@qq.com>
2020-05-21 15:52:56 +0800

    .

--- src/5.cpp
+++ src/5.cpp
@@ -1,58 +1,30 @@
-/*
- * @lc app=leetcode.cn id=5 lang=cpp
- *
- * [5] 最长回文子串
- *  输入: "babad"
-    输出: "bab"
-    注意: "aba" 也是一个有效答案。
- */
-
-// @lc code=start
-#include<iostream>
-#include <string>
-#include <vector>
-#include <cstring>
-
-using namespace std;
-
-
-
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size() == 0)
-            return "";
-        string result;
-        result.push_back(s[0]);
-        int len = s.size();
-        int P[len][len];
-        memset(P, 0, sizeof(P));
-        
-        for(int i = 0; i < len; ++i){
-            P[i][i] = 1;
-            if(i < len - 1){
-                if (s[i] == s[i+1]){
-                    P[i][i + 1] = 1;
-                    result = s.substr(i, 2);
-                }
-            }
-        }
+        if(s.empty()) return "";
+        int n = s.size();
+        vector<vector<bool>> f(n, vector<bool>(n, false));
+        int max_length = 1;
+        int res_i = 0, res_j = 0;
 
-        for(int gap = 2; gap < len; ++gap){
-            for (int i = 0; i < len - gap; ++i){
-                int j = i + gap;
-                if(s[i]==s[j]){
-                    P[i][j] = P[i + 1][j - 1];
-                    // cout << i << j << " ";
-                    // cout << s.substr(i, j - i + 1) << " " << P[i][j] << endl;
-                    if(P[i][j] && gap+1 > result.size()){
-                        result = s.substr(i, gap+1);
-                    }
+        for(int l = 0; l < n; l++){
+            for(int i = 0; i < n; i++){
+                int j = i + l;
+                if(j > n - 1) continue;
+
+                if(i == j) f[i][j] = true;
+                else if(i + 1 == j) f[i][j] = (s[i] == s[j]);
+                else f[i][j] = f[i + 1][j - 1] & (s[i] == s[j]);
+
+                if(f[i][j] && l + 1 > max_length){
+                    max_length = l + 1;
+                    res_i = i;
+                    res_j = j;
                 }
             }
+
         }
-        return result;
+        return s.substr(res_i, max_length);
     }
-};
-// @lc code=end
 
+};
