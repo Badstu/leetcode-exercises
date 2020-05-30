a82714b320dbb4cf2094315e97f0612c25b2b94b
sayhi <1045541554@qq.com>
2020-05-30 13:51:45 +0800

    .

--- src/198.cpp
+++ src/198.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        if(nums.size() == 1) return nums[0];
         int n = nums.size();
-        int f[n+1];
-        f[0] = 0;
-        f[1] = nums[1];
-        f[2] = max(nums[1], nums[2]);
-
-        for(int i = 3; i <= n; i++){
-            f[i] = max(f[i - 2] + nums[i - 1], f[i - 1]);
+        vector<int> f(n+1, 0);
+        
+        f[0] = nums[0];
+        f[1] = max(nums[0], nums[1]);
+  
+        for(int i = 2; i < n; i++){
+            f[i] = max(f[i-1], f[i-2] + nums[i]);
         }
-        return f[n];
+        return f[n-1];
     }
 };
